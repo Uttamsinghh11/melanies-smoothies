7024a812752d4f259cd0eccc0a6105c470117a3b
Uttamsinghh11 <uttamsinghhhh11@gmail.com>
2024-09-17 15:41:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
  
 # Write directly to the app
  
-streamlit.title('My Parents New Healthy Dinner')
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
